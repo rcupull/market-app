8c46acf478adffc3cec4eda56a5288a39fa4e7db
rcupull <59990075+rcupull@users.noreply.github.com>
2024-04-30 16:13:18 -0400

    Revert "update"
    This reverts commit 24c848331d47bcd63f8871f435298e32b87409fa.

--- back/src/index.ts
+++ back/src/index.ts
@@ -2,24 +2,12 @@ import { apiPort } from "./config";
 import { connectDB } from "./db";
 import { notificationsServices } from "./features/notifications";
 import { app } from "./server";
-import greenlockExpress from "greenlock-express";
+
+const PORT = apiPort || "4009";
 
 connectDB();
 notificationsServices.init();
 
-if (process.env.NODE_ENV === "development") {
-  const PORT = apiPort || "4009";
-
-  app.listen(PORT, () => {
-    console.log(`Server running at port ${PORT}`);
-  });
-} else {
-  greenlockExpress
-    .init({
-      packageRoot: process.cwd(),
-      configDir: "./greenlock.d",
-      maintainerEmail: "rcupull@gmail.com",
-      cluster: false,
-    })
-    .serve(app);
-}
+app.listen(PORT, () => {
+  console.log(`Server running at port ${PORT}`);
+});
