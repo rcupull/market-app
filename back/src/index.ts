24c848331d47bcd63f8871f435298e32b87409fa
rcupull <59990075+rcupull@users.noreply.github.com>
2024-04-29 21:22:54 -0400

    update

--- back/src/index.ts
+++ back/src/index.ts
@@ -2,12 +2,24 @@ import { apiPort } from "./config";
 import { connectDB } from "./db";
 import { notificationsServices } from "./features/notifications";
 import { app } from "./server";
-
-const PORT = apiPort || "4009";
+import greenlockExpress from "greenlock-express";
 
 connectDB();
 notificationsServices.init();
 
-app.listen(PORT, () => {
-  console.log(`Server running at port ${PORT}`);
-});
+if (process.env.NODE_ENV === "development") {
+  const PORT = apiPort || "4009";
+
+  app.listen(PORT, () => {
+    console.log(`Server running at port ${PORT}`);
+  });
+} else {
+  greenlockExpress
+    .init({
+      packageRoot: process.cwd(),
+      configDir: "./greenlock.d",
+      maintainerEmail: "rcupull@gmail.com",
+      cluster: false,
+    })
+    .serve(app);
+}
