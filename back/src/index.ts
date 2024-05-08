2499b0fc1d27f6aebb991ba064216c24f592717a
rcupull <59990075+rcupull@users.noreply.github.com>
2024-05-08 00:06:42 -0400

    update

--- back/src/index.ts
+++ back/src/index.ts
@@ -2,10 +2,41 @@ import { connectDB } from "./db";
 import { logger } from "./features/logger";
 import { notificationsServices } from "./features/notifications";
 import { app } from "./server";
+import fs from "fs";
+import https from "https";
+import http from "http";
 
 connectDB();
 notificationsServices.init();
 
-app.listen("80", () => {
-  logger.info(`Server running at port ${80}`);
+if (process.env.NODE_ENV === "production") {
+  const privateKey = fs.readFileSync(
+    "/etc/letsencrypt/live/aseremarket.net/privkey.pem",
+    "utf8"
+  );
+  const certificate = fs.readFileSync(
+    "/etc/letsencrypt/live/aseremarket.net/cert.pem",
+    "utf8"
+  );
+  const ca = fs.readFileSync(
+    "/etc/letsencrypt/live/aseremarket.net/chain.pem",
+    "utf8"
+  );
+
+  const credentials = {
+    key: privateKey,
+    cert: certificate,
+    ca: ca,
+  };
+
+  const httpsServer = https.createServer(credentials, app);
+
+  httpsServer.listen(443, () => {
+    logger.info("HTTPS Server running on port 443");
+  });
+}
+
+const httpServer = http.createServer(app);
+httpServer.listen(80, () => {
+  logger.info("HTTP Server running on port 80");
 });
