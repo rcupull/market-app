a6013eba8609b3fdec11b6ea82cfb5753d5a671b
rcupull <59990075+rcupull@users.noreply.github.com>
2024-05-09 14:53:20 -0400

    update

--- web/src/pages/@modals/useBusinessUpdatePostForm/Component.tsx
+++ web/src/pages/@modals/useBusinessUpdatePostForm/Component.tsx
@@ -1,3 +1,4 @@
+import { Badge } from 'components/badge';
 import { Button } from 'components/button';
 import { FieldCheckbox } from 'components/field-checkbox';
 import { FieldRadioGroup } from 'components/field-radio-group';
@@ -33,117 +34,109 @@ export const Component = ({ portal, onAfterSuccess, className }: ComponentProps)
   }
 
   return (
-    <Formik<State>
-      initialValues={{
-        postFormFields,
-      }}
-      onSubmit={() => {}}
-      enableReinitialize
-    >
-      {({ values, isValid, touched }) => {
-        return (
-          <form className={className}>
-            <FieldRadioGroup<{ disabled?: boolean; value: PostFormField; label: string }>
-              id="logoField"
-              name="postFormFields"
-              optionToValue={({ value }) => value}
-              disabledOption={({ item: { disabled } }) => !!disabled}
-              multi
-              renderOption={({ item, checked }) => {
-                const { disabled, label } = item;
-                return (
-                  <FieldCheckbox
-                    noUseFormik
-                    value={checked}
-                    label={disabled ? `${label} (requerido)` : label}
-                  />
-                );
-              }}
-              items={[
-                {
-                  label: 'Nombre del producto',
-                  value: 'name', //required
-                  disabled: true,
-                },
-                {
-                  label: 'Categorías',
-                  value: 'postCategoriesTags', //required
-                  disabled: true,
-                },
-                {
-                  label: 'Imágenes',
-                  value: 'images', //required
-                  disabled: true,
-                },
-                {
-                  label: 'Diseño de la página',
-                  value: 'postPageLayout',
-                },
-                {
-                  label: 'Moneda',
-                  value: 'currency',
-                },
-                {
-                  label: 'Tallas de ropa',
-                  value: 'clothingSizes',
-                },
-                {
-                  label: 'Colores',
-                  value: 'colors',
-                },
-                {
-                  label: 'Descripción',
-                  value: 'description',
-                },
-                {
-                  label: 'Detalles',
-                  value: 'details',
-                },
-                {
-                  label: 'Descuento',
-                  value: 'discount',
-                },
-                {
-                  label: 'Precio',
-                  value: 'price',
-                },
-                {
-                  label: 'Existencia en almacén',
-                  value: 'stockAmount',
-                },
-              ]}
-              className="mt-6"
-              containerClassName="grid  grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 gap-4"
-            />
+    <div>
+      <div className="flex flex-col md:flex-row items-center my-4">
+        <Badge variant="info" />
+        <span className="text-gray-600 ml-0  md:ml-4 mt-4 md:mt-0">
+          Estos son los campos del formulario de publicaciones que estarán disponibles a completar
+          durante la creación o edición de una publicación. Los campos{' '}
+          <span className="font-bold">Nombre del producto</span> ,{' '}
+          <span className="font-bold">Categorías</span> e{' '}
+          <span className="font-bold">Imágenes</span> no son mostrados porque son obligatorios y
+          siempre estarán en el formulario.
+        </span>
+      </div>
 
-            {portal.getPortal(
-              <Button
-                label="Guardar"
-                isBusy={updateOneBusiness.status.isBusy}
-                disabled={!isValid || isEmpty(touched)}
-                onClick={() => {
-                  if (!business) return;
-                  const { postFormFields } = values;
-
-                  updateOneBusiness.fetch(
-                    {
-                      update: {
-                        postFormFields,
-                      },
-                      routeName,
-                    },
-                    {
-                      onAfterSuccess,
-                    },
-                  );
+      <Formik<State>
+        initialValues={{
+          postFormFields,
+        }}
+        onSubmit={() => {}}
+        enableReinitialize
+      >
+        {({ values, isValid, touched }) => {
+          return (
+            <form className={className}>
+              <FieldRadioGroup<{ value: PostFormField; label: string }>
+                id="logoField"
+                name="postFormFields"
+                optionToValue={({ value }) => value}
+                multi
+                renderOption={({ item, checked }) => {
+                  const { label } = item;
+                  return <FieldCheckbox noUseFormik value={checked} label={label} />;
                 }}
-                variant="primary"
-                className="w-full"
-              />,
-            )}
-          </form>
-        );
-      }}
-    </Formik>
+                items={[
+                  {
+                    label: 'Diseño de la página',
+                    value: 'postPageLayout',
+                  },
+                  {
+                    label: 'Moneda',
+                    value: 'currency',
+                  },
+                  {
+                    label: 'Tallas de ropa',
+                    value: 'clothingSizes',
+                  },
+                  {
+                    label: 'Colores',
+                    value: 'colors',
+                  },
+                  {
+                    label: 'Descripción',
+                    value: 'description',
+                  },
+                  {
+                    label: 'Detalles',
+                    value: 'details',
+                  },
+                  {
+                    label: 'Descuento',
+                    value: 'discount',
+                  },
+                  {
+                    label: 'Precio',
+                    value: 'price',
+                  },
+                  {
+                    label: 'Existencia en almacén',
+                    value: 'stockAmount',
+                  },
+                ]}
+                className="mt-6"
+                containerClassName="grid  grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 gap-4"
+              />
+
+              {portal.getPortal(
+                <Button
+                  label="Guardar"
+                  isBusy={updateOneBusiness.status.isBusy}
+                  disabled={!isValid || isEmpty(touched)}
+                  onClick={() => {
+                    if (!business) return;
+                    const { postFormFields } = values;
+
+                    updateOneBusiness.fetch(
+                      {
+                        update: {
+                          postFormFields,
+                        },
+                        routeName,
+                      },
+                      {
+                        onAfterSuccess,
+                      },
+                    );
+                  }}
+                  variant="primary"
+                  className="w-full"
+                />,
+              )}
+            </form>
+          );
+        }}
+      </Formik>
+    </div>
   );
 };
