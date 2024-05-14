ad9e63e1faf38d04bb87b9a1159e12410ac17a76
rcupull <59990075+rcupull@users.noreply.github.com>
2024-05-14 07:22:34 -0400

    update

--- back/src/middlewares/frontMiddlware.ts
+++ back/src/middlewares/frontMiddlware.ts
@@ -109,9 +109,6 @@ export const frontMiddlware = combineMiddleware(
       "/docs",
     ];
 
-    logger.info("req.url");
-    logger.info(req.url);
-
     const isNotBusinessRoute =
       noBusinessRoutes
         .map((noBusinessRoute) => req.url.startsWith(noBusinessRoute))
