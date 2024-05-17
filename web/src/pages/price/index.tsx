ec773b9f8a0ae1dca4691e28eafe01aa866589ff
rcupull <59990075+rcupull@users.noreply.github.com>
2024-05-17 00:10:07 -0400

    update

--- web/src/pages/price/index.tsx
+++ web/src/pages/price/index.tsx
@@ -4,11 +4,11 @@ import { LayoutPage } from 'pages/@common/layout-page';
 
 export const Price = () => {
   return (
-    <LayoutPage title="Precio">
+    <LayoutPage title="Precios">
       <HtmlTextContainer>
         <p>
-          Los costos de los servicios de Asere Market dependen de las órdenes de compras realizadas
-          para cada negocio, siguiendo las siguientes pautas:
+          Asere Market garantiza el costos de sus servicios según las órdenes de compras realizadas
+          para cada negocio teniendo en cuenta las siguientes pautas:
         </p>
         <ol className="mt-4">
           <li className="my-2">
@@ -16,21 +16,16 @@ export const Price = () => {
             cada negocio.
           </li>
           <li className="my-2">
-            Asere Market le asigna un crédito inicial de 500 CUP a cada negocio durante su creación.
-            Con este crédito el propietario puede realizar hasta 25.000 CUP en ventas sin realizar
-            pago alguno por los recursos del sistema. De esta forma los propietarios pueden insertar
-            sus productos y probar las ventajas del sistema sin abonarle nada a Asere Market.
+            ⁠Asere Market le asigna un crédito inicial de 500 CUP a cada negocio durante su
+            creación, garantizando al propietario una ganancia mínima 25.000 CUP en ventas sin
+            realizar pago alguno por los recursos asignados, de esta forma los propietarios pueden
+            insertar sus productos y probar las ventajas del sistema.
           </li>
 
           <li className="my-2">
-            Una vez agotado el crédito inicial cada propietario debe comprar los créditos que
-            necesite para mantener sus ventas en cada negocio.
-          </li>
-
-          <li className="my-2">
-            Al agotarse el crédito, ya sea el inicial o el comprado por el propietario, Asere Market
-            informará a cada propietario dando una margen de 7 días para comprar más créditos y
-            mantener la puesta en linea de sus nogocios.
+            Una vez agotado el crédito inicial, siendo previamente informado 7 dias antes, el
+            propietario debe comprar los créditos que necesite para mantener la puesta en linea de
+            sus nogocios.
           </li>
 
           <li className="my-2">
@@ -39,10 +34,11 @@ export const Price = () => {
             proceso Asere Market posee un sistema de control de disponibilidad en almacén que el
             propietario puede habilitar en sus productos.
           </li>
+
           <li className="my-2">
             El sistema se resposabiliza de “bannar” a los usuario que reiteradamente generen órdenes
             de compra y no efectuen ventas satisfactorias protegiendo asi el flujo de ventas de cada
-            negocio.
+            negocio
           </li>
         </ol>
       </HtmlTextContainer>
