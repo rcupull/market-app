b154d9615bcfa644073fed336f399009ebe8520f
rcupull <59990075+rcupull@users.noreply.github.com>
2024-06-01 00:04:20 -0400

    update

--- web/src/pages/dashboard/routes/business/routes/route-name/components/BusinessConfig/index.tsx
+++ web/src/pages/dashboard/routes/business/routes/route-name/components/BusinessConfig/index.tsx
@@ -65,7 +65,7 @@ export const BusinessConfig = ({ business }: BusinessConfigProps) => {
     return () => {
       interval.cancel();
     };
-  }, []);
+  }, [business.routeName]);
 
   return <></>;
 };
