5f5392430fe901b62bd99004583df26112975d1b
rcupull <59990075+rcupull@users.noreply.github.com>
2024-06-18 13:13:25 -0400

    update

--- back/src/features/admin/index.ts
+++ back/src/features/admin/index.ts
@@ -5,7 +5,6 @@ import { router as shoppingRouter } from './shopping/routes';
 import { router as businessRouter } from './business/routes';
 import { router as usersRouter } from './users/routes';
 import { router as billsRouter } from './bills/routes';
-import { isAdmin, isLogged } from '../../middlewares/verify';
 
 export const router = Router();
 
