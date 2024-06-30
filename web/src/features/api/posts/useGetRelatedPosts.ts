30fc1e6f836262b297996778917f9bd8525eb922
CfM47 <jossuearteche000@gmail.com>
2024-06-30 17:51:29 -0400

    refactor: Update useGetRelatedPosts to use paginated data

--- web/src/features/api/posts/useGetRelatedPosts.ts
+++ web/src/features/api/posts/useGetRelatedPosts.ts
@@ -1,17 +1,18 @@
 import { useFetch } from 'hooks/useFetch';
 
-import { FetchResourceWithPagination } from 'types/api';
+import { FetchResourceWithPagination, PaginatedData } from 'types/api';
 import { Post } from 'types/post';
 import { getEndpoint } from 'utils/api';
+import { getPaginationResources } from 'utils/pagination';
 
 export const useGetRelatedPosts = (): {
   getRelatedPosts: FetchResourceWithPagination<{ id: string }, Post>;
 } => {
-  const fetch = useFetch<Post[]>();
+  const fetch = useFetch<PaginatedData<Post>>();
 
   return {
     getRelatedPosts: {
-      data: fetch[0],
+      ...getPaginationResources(fetch[0]),
       status: fetch[1],
       fetch: ({ id }) => {
         fetch[2](
