4caf62df5c5880d8ec02efe71de306ea804fa5cb
rcupull <59990075+rcupull@users.noreply.github.com>
2024-07-06 10:50:55 -0400

    update

--- back/src/middlewares/frontMiddlware.ts
+++ back/src/middlewares/frontMiddlware.ts
@@ -14,6 +14,8 @@ import { HtmlMeta } from '../types/general';
 
 import { postServicesGetOne } from '../features/post/services';
 import { businessServicesFindOne } from '../features/business/services';
+import { shoppingServicesGetOne } from '../features/shopping/services';
+import { getShoppingStateLabel } from '../utils/shopping';
 
 const defaultMeta: HtmlMeta = {
   title: 'Asere Market - Comercio para todos',
@@ -144,11 +146,63 @@ const injectPostMetaMiddlware: RequestHandler = async (req, res, next) => {
   injectMetaService(req, res, next);
 };
 
+const injectShoppingMetaMiddlware: RequestHandler = async (req, res, next) => {
+  const { params } = req;
+  const { routeName, shoppingId } = params;
+  req.htmlMeta = defaultMeta;
+
+  if (!shoppingId) return injectMetaService(req, res, next);
+
+  const shopping = await shoppingServicesGetOne({
+    query: {
+      _id: shoppingId,
+    },
+  });
+
+  if (!shopping) {
+    return getPostNotFoundResponse({ res });
+  }
+
+  const products = shopping.posts;
+
+  const getImageSrc = () => {
+    const src = products[0].postData.images?.[0]?.src;
+
+    if (!src) {
+      return defaultMeta.image;
+    }
+
+    return src.startsWith('http') ? src : `${hostname}${src}`;
+  };
+
+  const getShoppingUrl = () => {
+    return `${hostname}/b/${routeName}/shopping/${shoppingId}`;
+  };
+
+  const getDescription = () => {
+    if (products.length === 1) {
+      return `Contiene ${products[0].postData.name}.`;
+    }
+
+    return `Contiene ${products[0].postData.name} y otros productos.`;
+  };
+
+  req.htmlMeta = {
+    description: getDescription(),
+    title: `Órden de compra en estado ${getShoppingStateLabel(shopping.state)}`,
+    image: getImageSrc(),
+    url: getShoppingUrl(),
+  };
+
+  injectMetaService(req, res, next);
+};
+
 const router = Router();
 
 export const frontMiddlware = combineMiddleware(
   router.get(/\/*(.png|.css|.js)/, express.static(join(process.cwd(), appFrontDir))),
   router.get('/b/:routeName/posts/:postId', injectPostMetaMiddlware),
+  router.get('/b/:routeName/shopping/:shoppingId', injectShoppingMetaMiddlware),
   router.get('/b/:routeName*', injectBusinessMetaMiddlware),
-  injectDefaultMetaMiddlware
+  injectDefaultMetaMiddlware,
 );
