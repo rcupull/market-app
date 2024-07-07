87e8102034b4e1d8b7919850011f448a1b8b94be
rcupull <59990075+rcupull@users.noreply.github.com>
2024-07-07 12:42:14 -0400

    update

--- back/src/features/shopping/handles.ts
+++ back/src/features/shopping/handles.ts
@@ -309,7 +309,7 @@ const post_shopping_shoppingId_make_order: () => RequestHandler = () => {
           purchaserId: user._id,
         },
         update: {
-          state: 'REQUESTED',
+          state: ShoppingState.REQUESTED,
         },
       });
 
@@ -349,7 +349,7 @@ const post_shopping_shoppingId_change_state: () => RequestHandler = () => {
 
       const state: ShoppingState = body.state;
 
-      if (state === 'CONSTRUCTION') {
+      if (state === ShoppingState.CONSTRUCTION) {
         return get400Response({
           res,
           json: { message: 'Can not change the state to CONSTRUCTION' },
@@ -364,7 +364,7 @@ const post_shopping_shoppingId_change_state: () => RequestHandler = () => {
         return getShoppingNotFoundResponse({ res });
       }
 
-      if (shopping.state === 'CONSTRUCTION') {
+      if (shopping.state === ShoppingState.CONSTRUCTION) {
         return get400Response({
           res,
           json: { message: 'Can not change the state from CONSTRUCTION' },
