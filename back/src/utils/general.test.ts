338fc0b95fd6882c18718309bac2ec78e4be4904
rcupull <59990075+rcupull@users.noreply.github.com>
2024-07-08 08:36:47 -0400

    update

--- back/src/utils/general.test.ts
+++ back/src/utils/general.test.ts
@@ -1,4 +1,4 @@
-import { movRow } from './general';
+import { movRow, stringExtract } from './general';
 
 describe('movRow()', () => {
   it('should return a right value 1', async () => {
@@ -23,5 +23,17 @@ describe('movRow()', () => {
   "b",
 ]
 `);
+  });
+});
+
+describe('stringExtract()', () => {
+  it.each([
+    [['name1'], 'products.name.{val}', 'products.name.name1'],
+    [null, 'products.name.{val}', 'business.name.name1'],
+    [null, 'products.name.{val}', 'products.name.name1.category.cat1'],
+    [['name1', 'cat1'], 'products.name.{val}.category.{val}', 'products.name.name1.category.cat1'],
+    [null, 'products.name.{val}.category.{val}', 'products.name.name1'],
+  ])('should return %p when value = %p', (expected, exp, value) => {
+    expect(stringExtract(exp, value)).toEqual(expected);
   });
 });
