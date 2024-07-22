8b3072f8f3d85373cdfbb848cdd5acb87178b35f
rcupull <59990075+rcupull@users.noreply.github.com>
2024-07-22 18:41:36 -0400

    update

---
+++ web/src/pages/@modals/useBusinessShoppingTermsAndConditionsModal/index.test.ts
@@ -0,0 +1,133 @@
+import { act, renderHook, screen, waitFor } from '@testing-library/react';
+
+import { useBusinessShoppingTermsAndConditionsModal } from '.';
+
+import { getWrapper } from 'utils/test-utils';
+
+describe('useBusinessShoppingTermsAndConditionsModal', () => {
+  it('render', async () => {
+    const { result } = renderHook(() => useBusinessShoppingTermsAndConditionsModal(), {
+      wrapper: getWrapper({ useRouter: true, useModal: true }),
+    });
+
+    act(() => result.current.open());
+
+    await waitFor(() =>
+      expect(screen.getByTestId('Modal')).toMatchInlineSnapshot(`
+        <div
+          aria-modal="true"
+          class="relative z-30"
+          data-headlessui-state="open"
+          data-open=""
+          data-testid="Modal"
+          id="headlessui-dialog-:r0:"
+          role="dialog"
+          tabindex="-1"
+        >
+          <div
+            class="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity"
+          />
+          <div
+            class="fixed inset-0 z-10 w-screen overflow-y-auto"
+          >
+            <div
+              class="flex min-h-full items-center justify-center p-4 text-center sm:items-center sm:p-0"
+            >
+              <div
+                class="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 w-11/12 md:w-8/12 lg:w-8/12"
+                data-headlessui-state="open"
+                data-open=""
+                id="headlessui-dialog-panel-:r7:"
+              >
+                <button
+                  class="relative px-3 py-1.5 text-sm shadow-sm font-semibold rounded-md flex items-center justify-center leading-6 whitespace-nowrap h-fit bg-transparent text-gray-600 fill-gray-600 ring-2 ring-gray-300 hover:bg-gray-100 !rounded-full !p-2 !ring-0 !shadow-none !absolute top-0 right-0"
+                >
+                  <svg
+                    class="h-5 w-5"
+                    height="1em"
+                    viewBox="0 0 32 32"
+                    width="1em"
+                  >
+                    <path
+                      d="M7.219 5.781L5.78 7.22 14.563 16 5.78 24.781 7.22 26.22 16 17.437l8.781 8.782 1.438-1.438L17.437 16l8.782-8.781L24.78 5.78 16 14.563z"
+                    />
+                  </svg>
+                </button>
+                <div
+                  class="bg-white px-4 pb-4 pt-5 sm:p-6 sm:pb-4"
+                >
+                  <div
+                    class="sm:flex sm:items-start w-full"
+                  >
+                    <div
+                      class="mx-9 sm:mx-2 flex justify-center"
+                    >
+                      <div
+                        class="flex size-12 flex-shrink-0 items-center justify-center rounded-full bg-gray-200"
+                      >
+                        <svg
+                          aria-hidden="true"
+                          class="size-8 fill-gray-600"
+                          height="1em"
+                          viewBox="0 0 32 32"
+                          width="1em"
+                        >
+                          <path
+                            d="M5 7c-.55 0-1 .45-1 1s.45 1 1 1h2.219l2.625 10.5c.222.89 1.02 1.5 1.937 1.5H23.25c.902 0 1.668-.598 1.906-1.469L27.75 10H11l.5 2h13.656l-1.906 7H11.781L9.156 8.5A1.983 1.983 0 007.22 7zm17 14c-1.645 0-3 1.355-3 3s1.355 3 3 3 3-1.355 3-3-1.355-3-3-3zm-9 0c-1.645 0-3 1.355-3 3s1.355 3 3 3 3-1.355 3-3-1.355-3-3-3zm0 2c.563 0 1 .438 1 1 0 .563-.438 1-1 1-.563 0-1-.438-1-1 0-.563.438-1 1-1zm9 0c.563 0 1 .438 1 1 0 .563-.438 1-1 1-.563 0-1-.438-1-1 0-.563.438-1 1-1z"
+                          />
+                        </svg>
+                      </div>
+                    </div>
+                    <div
+                      class="mt-3 sm:ml-4 sm:mt-0 w-full"
+                    >
+                      <h3
+                        class="text-xl font-semibold text-gray-900"
+                        data-headlessui-state="open"
+                        data-open=""
+                        id="headlessui-dialog-title-:r8:"
+                      >
+                        Términos y condiciones para la venta de productos
+                      </h3>
+                      <div
+                        class="mt-4"
+                      >
+                        <div
+                          class="flex justify-center items-center fixed inset-0 z-50"
+                        >
+                          <div>
+                            <div
+                              class="rcs-ellipsis"
+                              style="--rcs-ellipsis-color: rgba(0,0,0, 0.5);"
+                            >
+                              <div />
+                              <div />
+                              <div />
+                              <div />
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div
+                  class="bg-gray-50 px-4 py-3 sm:flex sm:flex-row sm:px-6"
+                >
+                  <div
+                    class="w-full sm:w-auto ml-auto"
+                  />
+                  <button
+                    class="relative px-3 py-1.5 text-sm shadow-sm font-semibold rounded-md flex items-center justify-center leading-6 whitespace-nowrap h-fit bg-transparent text-gray-600 fill-gray-600 ring-2 ring-gray-300 hover:bg-gray-100 w-full sm:w-auto mt-3 sm:mt-0 ml-0 sm:ml-3"
+                  >
+                    Cerrar
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      `)
+    );
+  });
+});
